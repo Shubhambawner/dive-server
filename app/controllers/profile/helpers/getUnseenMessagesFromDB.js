b1a6ae324800c7d14a9543e358e2117c2c416ac7
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2023-04-02 03:22:31 +0530

    getUnseenMessagesFromDb implemented...

--- app/controllers/profile/helpers/getUnseenMessagesFromDB.js
+++ app/controllers/profile/helpers/getUnseenMessagesFromDB.js
@@ -1,15 +1,25 @@
 const Chat = require('../../../models/chat')
-const { itemNotFound } = require('../../../middleware/utils')
+const { itemNotFound, isIDGood } = require('../../../middleware/utils')
+const { getItems } = require('../../../middleware/db')
 
 /**
  * Gets profile from database by id
  * @param {string} id - user id
  */
-const getUnseenMessagesFromDB = (userId = '') => {
+const getUnseenMessagesFromDB = async (userId = '') => {
   // todo complete this: high priority
-  return new Promise((resolve,reject)=>{
-    resolve([])
-  })
+  try {
+    const query = {
+      receiverId: userId,
+      status: 'unseen'
+    }
+    let data = await getItems({query:{page:1,limit:100}}, Chat, query)
+    return data.docs || []
+  } catch (error) {
+    console.error(error);
+    return []
+  }
+
 }
 
 module.exports = { getUnseenMessagesFromDB }
