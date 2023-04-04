5da330cd1a3a82acbf8f71bf9fb7940150dc381b
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2023-04-04 19:46:59 +0530

    removed constants.js since now window.config works...

--- public/js/constants.js
+++
@@ -1,6 +0,0 @@
-let isDev = true
-let url_prod = "https://dive-server.shubhambawner.repl.co/"
-let url_dev = "http://localhost:3000/"
-let serverUrl = isDev ? url_dev : url_prod
-
-export { serverUrl }
